096c7291080c837d35c7e07757b6a160371bb675
Pham Anh Tuan <phamanhtuan2833@gmail.com>
2024-07-31 00:32:00 +0700

    refactor: :recycle: refactor data

--- src/utils/data/personal-data.js
+++ src/utils/data/personal-data.js
@@ -3,7 +3,7 @@ export const personalData = {
   profile: "/profile.jpg",
   designation: "Web Developer",
   description:
-    "My name is Pham Anh Tuan, and I am a passionate programmer who thrives on learning and exploring new technologies. My primary expertise lies in JavaScript, and I enjoy leveraging it to accomplish a wide range of tasks. I am always eager to expand my knowledge and skill set, continuously seeking out new challenges and opportunities to grow myself.",
+    "My name is Pham Anh Tuan, I am a passionate programmer who thrives on learning and exploring new technologies. My primary expertise lies in JavaScript, and I enjoy leveraging it to accomplish a wide range of tasks. I am always eager to expand my knowledge and skill set, continuously seeking out new challenges and opportunities to grow myself.",
   email: "phamanhtuan2833@gmail.com",
   phone: "0987 957 355",
   address: "My Dinh, Nam Tu Liem, Ha Noi",
